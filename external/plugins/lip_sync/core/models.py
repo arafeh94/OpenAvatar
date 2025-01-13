2bcc070e1a46755c9a3f728add815c6e4593ec3e
Mouhamad Arafeh <arafeh198@gmail.com>
2025-01-13 15:28:16 -0500

    add dynamic avatar model path

--- external/plugins/lip_sync/core/models.py
+++ external/plugins/lip_sync/core/models.py
@@ -2,15 +2,17 @@ from onnx2torch import convert
 import torch
 
 from external.core.interfaces.base_ai import NonBlockingAIModel
+from manifest import Manifest
 
 
 class AvatarWave2LipModel(NonBlockingAIModel):
     def __init__(self):
         self.device = "cuda" if torch.cuda.is_available() else 'cpu'
+        self.model_path = Manifest().get('avatar')['gan_model_path']
         super().__init__()
 
     def load_model(self):
-        onnx_model_path = "/home/arafeh/PycharmProjects/avatar_rag_back_end/avatar/checkpoints/wav2lip_gan_96.onnx"
+        onnx_model_path = self.model_path
         model = convert(onnx_model_path)
         model = model.to(self.device)
         return model.eval()
